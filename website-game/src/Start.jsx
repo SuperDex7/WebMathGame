36304bccd521aeba9f45c935ba3bdd1f6eee8b16
SuperDex7 <superdexter7@gmail.com>
2023-12-31 13:33:15 -0500

    new buttons

---
+++ website-game/src/Start.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+function Start() {
+    const [show, setShow] = useState(true);
+    function StartGame(){
+
+    }
+    return (  
+        <button id="start">Start</button>
+    );
+}
+
+export default Start;
---
+++ website-game/src/difficulty.jsx
@@ -0,0 +1,13 @@
+function Difficulty(){
+    
+    return(
+    <div>
+
+        <button id="easy">Easy</button>
+        <button id="medium">Medium</button>
+        <button id="hard">Hard</button>
+    </div>
+    
+);
+}
+export default Difficulty
